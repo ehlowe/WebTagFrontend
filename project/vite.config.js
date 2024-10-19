d30d3668afb0301d98c9f49285e378babcf97f08
Ethan Lowe <104944142+MrJellyBean3@users.noreply.github.com>
2024-10-18 23:45:17 -0500

    domain change

--- project/vite.config.js
+++ project/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.mp3'],
-  base: '/WebTagFrontend/',
+  // base: '/WebTagFrontend/',
   build: {
     outDir: '../'
   }
